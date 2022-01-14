57f1f1fa0187ca5a7c2b64cc95efcb7c0b63d137
Neutrallice7 <sulthanprabowo7@gmail.com>
2022-01-14 11:33:12 +0700

    Add files

---
+++ Problem2.js
@@ -0,0 +1,11 @@
+// getCost 
+// getPrice 
+// getName
+
+var ingredients = ['Strawberries', 'Banana', 'Mango', 'Blueberries', 'Raspberries', 'Apple', 'Pineapple'];
+
+function smoothie(n) {
+    if (ingredients[n]) {
+        ingredients[n];
+    }
+}
